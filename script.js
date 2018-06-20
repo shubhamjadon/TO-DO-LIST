dce4d99fb773ad7010005a718f70adcb583370c1
unknown <shubhamjadon99@gmail.com>
2018-06-20 19:34:48 +0530

    With delete Feature

--- script.js
+++ script.js
@@ -9,12 +9,28 @@ btnAdd.addEventListener('click', () => {
     data.push(input.value);
     input.value = '';
     makeList();
+    addEvents();
   }
 });
-
 function makeList() {
   list.innerHTML = "";
-  data.forEach(task => {
-    list.innerHTML += "<li><h3>" + task + "</h3></li>"
+  for(let i=0; i<data.length; i++) {
+    list.innerHTML += ("<li><h3>" + data[i] + "</h3></li>");
+  }
+}
+
+function addEvents() {
+  let tasks = document.querySelectorAll("li");
+  tasks.forEach(task => {
+    task.addEventListener("click", function() {
+      if(this.classList[0] != "cross") {
+        this.classList.add("cross");
+      } else {
+        let index = data.indexOf(this.textContent);
+        data.splice(index,1);
+        makeList();
+        addEvents();
+      }
+    });
   });
 }
